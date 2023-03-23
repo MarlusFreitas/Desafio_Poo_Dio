2b24a90487cd400528d8cee6f3c848602ce9c736
Marlus <marlus_fox@hotmail.com>
2023-03-23 19:09:17 -0300

    Final

--- Conteúdo/Dev.java
+++ Conteúdo/Dev.java
@@ -1,10 +1,92 @@
 package Conteúdo;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
     private String nome;
     private Set<Conteúdo> conteudosInscritos = new LinkedHashSet<>();
-    private Set<Conteúdo> conteudoConcluido = new LinkedHashSet<>();
+    private Set<Conteúdo> conteudoConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteúdo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else{
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp(){
+        return this.conteudoConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteúdo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteúdo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteúdo> getConteudoConcluidos() {
+        return conteudoConcluidos;
+    }
+
+    public void setConteudoConcluido(Set<Conteúdo> conteudoConcluido) {
+        this.conteudoConcluidos = conteudoConcluido;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudosInscritos == null) ? 0 : conteudosInscritos.hashCode());
+        result = prime * result + ((conteudoConcluidos == null) ? 0 : conteudoConcluidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudosInscritos == null) {
+            if (other.conteudosInscritos != null)
+                return false;
+        } else if (!conteudosInscritos.equals(other.conteudosInscritos))
+            return false;
+        if (conteudoConcluidos == null) {
+            if (other.conteudoConcluidos != null)
+                return false;
+        } else if (!conteudoConcluidos.equals(other.conteudoConcluidos))
+            return false;
+        return true;
+    }
+
 }
--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import Conteúdo.Bootcamp;
 import Conteúdo.Curso;
+import Conteúdo.Dev;
 import Conteúdo.Mentoria;
 
 public class main {
@@ -20,9 +22,42 @@ public class main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setName("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println(("Conteúdos Inscritos") + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println(("Conteúdos Inscritos") + devCamila.getConteudosInscritos());
+        System.out.println(("Conteúdos Concluidoss") + devCamila.getConteudoConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+        
+        System.out.println("-----------");
+        
+        Dev devJoao = new Dev();
+        devJoao.setNome("joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println(("Conteúdos Inscritos") + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println(("Conteúdos Inscritos") + devJoao.getConteudosInscritos());
+        System.out.println(("Conteúdos Concluidos") + devJoao.getConteudoConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+        
     }
 }
